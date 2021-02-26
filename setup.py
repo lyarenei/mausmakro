0fc0ccb963c8eadc258cd8697fee1e9c0d4aac8f
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2021-02-26 21:16:14 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+setuptools.setup(
+    name='mausmakro',
+    version='0.1.0',
+    description='Mouse macro player for automating various tasks.',
+    url='https://github.com/lyarenei/mausmakro',
+    author='Dominik Krivohlavek',
+    author_email='domkrivohlavek@gmail.com',
+    license='None',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'lark',
+        'pyautogui',
+        'pynput',
+        'click',
+        'Pillow',
+        'opencv-python',
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mausmakro=mausmakro.__main__:main',
+        ],
+    },
+)
