f28cc3ff72f637c70817cca56db694822b079e30
Dominik Krivohlavek <domkrivohlavek@gmail.com>
2021-03-04 23:23:07 +0100

    Bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mausmakro',
-    version='0.1.0',
+    version='0.2.0',
     description='Mouse macro player for automating various tasks.',
     url='https://github.com/lyarenei/mausmakro',
     author='Dominik Krivohlavek',
